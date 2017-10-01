2a8264db4133a1e326054282afb47e32edd94c49
Genert Org <gnert1111studio@gmail.com>
2017-10-01 03:54:15 +0300

    build: tooling update

--- README.md
+++ README.md
@@ -9,13 +9,13 @@ More than 2 years later, I deem this project as commercial failure in over-satur
 
 Many mistakes were made in both business and development sense as I was the only one developing and designing this. Some of these mistakes were:
 1) Hunting perfectionism - I cannot tell how many times I felt that this product was not ready for public. Thinking back, I was wrong. It was, I was just hunting perfectionism: trying to write perfect code, use latest technology, implement certain new features, and so on. I would say release MVP as soon as possible so you can actually implement features that users want, not what you think they want.
-2) Timing is everything - at that time, when I started writing first line of this product, there was perhaps few such products but they all sucked at the niche I was targeting. Due "hunting perfectionism", the product release was delayed for 1.5 years (I am serious) and thus I was late to the game.
+2) Timing is everything - at that time, when I started writing first line of this product, there was perhaps few such products but they all sucked at the niche I was targeting. Due "hunting perfectionism", the product release was delayed for 1.5 years (I am serious) and thus I were late to the game.
 
 Of-course there are many other mistakes but I think these two are most important takeaways.
 
-The code was written back in 2015 and I am glad that I choose React with Redux that time as this project shows. Of-course there are many things I would do differently and in better way when I would start again, but I am overall happy how it turned out except for stylings which are quite horrible. I would definetly use CSS modules as that would make code much understandable.
+The code was written back in 2015 and I am glad that I choose React with Redux that time as this project shows. Of-course there are many things I would do differently and in better way when I would start again, but I am overall happy how it turned out except for stylings. I would definitely use CSS modules as that would make code much understandable and easier to work with.
 
-I hope this project (~30k LOC) shows what you can create with React and Redux and both inspires and show what kind of code not to write.
+I hope this project (~30k LOC) shows what you can create with React and Redux and both inspires and shows what kind of code to write and not.
 
 Cheers!
 
--- scripts/gulpfile.babel.js
+++ scripts/gulpfile.babel.js
@@ -44,209 +44,205 @@ function createErrorHandler(name) {
 // Rewrite gulp.src for better error handling.
 const gulpSrc = gulp.src;
 gulp.src = function (...args) {
-  return gulpSrc(...args)
-    .pipe($plumber(function (error) {
-      const { plugin, message } = error;
-      $util.log($util.colors.red(`Error (${plugin}): ${message}`));
-      this.emit('end');
-    }));
+    return gulpSrc(...args)
+        .pipe($plumber(function (error) {
+            const { plugin, message } = error;
+            $util.log($util.colors.red(`Error (${plugin}): ${message}`));
+            this.emit('end');
+        }));
 };
 
 // Create server.
 gulp.task('server', () => {
-  browserSync.init(config.server);
+    browserSync.init(config.server);
 });
 
 // Images task.
 gulp.task('images', () => {
-  return gulp.src(config.images.entry)
-    .pipe($changed(config.images.output))
-    .pipe($imagemin())
-    .pipe($size({ title: '[files:template]', gzip: true }))
-    .pipe(gulp.dest(config.images.output));
+    return gulp.src(config.images.entry)
+        .pipe($changed(config.images.output))
+        .pipe($imagemin())
+        .pipe($size({ title: '[files:template]', gzip: true }))
+        .pipe(gulp.dest(config.images.output));
 });
 
 // Compiles and deploys main stylesheet.
 gulp.task('stylesheet:main', () => {
-  if (config.env.debug) {
-    return gulp.src(config.stylesheets.main.entry)
-      .pipe($sass(config.stylesheets.sass).on('error', $sass.logError))
-      .pipe($size({ title: '[stylesheet:main]', gzip: true }))
-      .pipe(gulp.dest(config.stylesheets.main.output))
-      .pipe(browserSync.stream({ match: '**/*.css' }));
-  }
+    if (config.env.debug) {
+        return gulp.src(config.stylesheets.main.entry)
+            .pipe($sass(config.stylesheets.sass).on('error', $sass.logError))
+            .pipe($size({ title: '[stylesheet:main]', gzip: true }))
+            .pipe(gulp.dest(config.stylesheets.main.output))
+            .pipe(browserSync.stream({ match: '**/*.css' }));
+    }
 
-  return gulp.src(config.stylesheets.main.entry)
-    .pipe($sass(config.stylesheets.sass).on('error', $sass.logError))
-    .pipe($cleanCSS({ compatibility: 'ie8' }))
-    .pipe($autoprefixer(config.stylesheets.autoprefixer))
-    .pipe($size({ title: '[stylesheet:main]', gzip: true }))
-    .pipe(gulp.dest(config.stylesheets.main.output));
+    return gulp.src(config.stylesheets.main.entry)
+        .pipe($sass(config.stylesheets.sass).on('error', $sass.logError))
+        .pipe($cleanCSS({ compatibility: 'ie8' }))
+        .pipe($autoprefixer(config.stylesheets.autoprefixer))
+        .pipe($size({ title: '[stylesheet:main]', gzip: true }))
+        .pipe(gulp.dest(config.stylesheets.main.output));
 });
 
 // Compiles and deploys canvas stylesheet.
 gulp.task('stylesheet:canvas', () => {
-  if (config.env.debug) {
-    return gulp.src(config.stylesheets.canvas.entry)
-      .pipe($sass(config.stylesheets.sass).on('error', $sass.logError))
-      .pipe($size({ title: '[stylesheet:canvas]', gzip: true }))
-      .pipe(gulp.dest(config.stylesheets.canvas.output))
-      .pipe(browserSync.stream({ match: '**/*.css' }));
-  }
+    if (config.env.debug) {
+        return gulp.src(config.stylesheets.canvas.entry)
+            .pipe($sass(config.stylesheets.sass).on('error', $sass.logError))
+            .pipe($size({ title: '[stylesheet:canvas]', gzip: true }))
+            .pipe(gulp.dest(config.stylesheets.canvas.output))
+            .pipe(browserSync.stream({ match: '**/*.css' }));
+    }
 
-  return gulp.src(config.stylesheets.canvas.entry)
-    .pipe($sass(config.stylesheets.sass).on('error', $sass.logError))
-    .pipe($cleanCSS({ compatibility: 'ie8' }))
-    .pipe($size({ title: '[stylesheet:canvas]', gzip: true }))
-    .pipe(gulp.dest(config.stylesheets.canvas.output));
+    return gulp.src(config.stylesheets.canvas.entry)
+        .pipe($sass(config.stylesheets.sass).on('error', $sass.logError))
+        .pipe($cleanCSS({ compatibility: 'ie8' }))
+        .pipe($size({ title: '[stylesheet:canvas]', gzip: true }))
+        .pipe(gulp.dest(config.stylesheets.canvas.output));
 });
 
 // Compiles and deploys files.
 gulp.task('files', () => {
-  return gulp.src(config.files.entry)
-    .pipe($size({ title: '[files]', gzip: true }))
-    .pipe(gulp.dest(config.files.output));
+    return gulp.src(config.files.entry)
+        .pipe($size({ title: '[files]', gzip: true }))
+        .pipe(gulp.dest(config.files.output));
 });
 
 // Compiles and deploys template files.
 gulp.task('files:template', () => {
-  return gulp.src(config.files.template.entry)
-    .pipe($size({ title: '[files:template]', gzip: true }))
-    .pipe(gulp.dest(config.files.template.output));
+    return gulp.src(config.files.template.entry)
+        .pipe($size({ title: '[files:template]', gzip: true }))
+        .pipe(gulp.dest(config.files.template.output));
 });
 
 // Compiles and deploys template thumbnail files.
 gulp.task('files:template:thumbnail', () => {
-  return gulp.src(config.files.template.thumbnail.entry)
-    .pipe($changed(config.images.output))
-    .pipe($imagemin())
-    .pipe($size({ title: '[files:template:thumbnail]', gzip: true }))
-    .pipe(gulp.dest(config.files.template.thumbnail.output));
+    return gulp.src(config.files.template.thumbnail.entry)
+        .pipe($changed(config.images.output))
+        .pipe($imagemin())
+        .pipe($size({ title: '[files:template:thumbnail]', gzip: true }))
+        .pipe(gulp.dest(config.files.template.thumbnail.output));
 });
 
 // Compiles and deploys HTML files.
 gulp.task('html', () => {
-  if (config.env.debug) {
-    return gulp.src(config.html.entry)
-      .pipe($size({ title: '[html]', gzip: true }))
-      .pipe(gulp.dest(config.html.output));
-  }
+    if (config.env.debug) {
+        return gulp.src(config.html.entry)
+            .pipe($size({ title: '[html]', gzip: true }))
+            .pipe(gulp.dest(config.html.output));
+    }
 
-  return gulp.src(config.html.entry)
-    .pipe($size({ title: '[html]', gzip: true }))
-    .pipe(gulp.dest(config.html.output));
+    return gulp.src(config.html.entry)
+        .pipe($size({ title: '[html]', gzip: true }))
+        .pipe(gulp.dest(config.html.output));
 });
 
 // Revision
 gulp.task('rev', (callback) => {
-  gulp.src(config.rev.entry)
-    .pipe($rev())
-    .pipe(gulp.dest(config.rev.output))
-    .pipe($rev.manifest(config.rev.manifestFile))
-    .pipe(gulp.dest(config.rev.output))
-    .on('end', () => {
-      const manifestFile = path.join(config.rev.output, config.rev.manifestFile);
-      const manifest = require(manifestFile);
-      const removables = [];
-      const pattern = (_keys(manifest)).join('|');
+    return gulp.src(config.rev.entry)
+        .pipe($rev())
+        .pipe(gulp.dest(config.rev.output))
+        .pipe($rev.manifest(config.rev.manifestFile))
+        .pipe(gulp.dest(config.rev.output))
+        .on('end', () => {
+            const manifestFile = path.join(config.rev.output, config.rev.manifestFile);
+            const manifest = require(manifestFile);
+            const removables = [];
+            const pattern = (_keys(manifest)).join('|');
 
-      for (const v in manifest) {
-        if (v !== manifest[v]) {
-          removables.push(path.join(config.rev.output, v));
-        }
-      }
+            for (const v in manifest) {
+                if (v !== manifest[v]) {
+                    removables.push(path.join(config.rev.output, v));
+                }
+            }
 
-      removables.push(manifestFile);
+            removables.push(manifestFile);
 
-      rimraf(`{${removables.join(',')}}`)
-        .then(() => {
-          if (!_isEmpty(config.cdn)) {
-            gulp.src(config.rev.replace)
-              .pipe($replace(new RegExp(`((?:\\.?\\.\\/?)+)?([\\/\\da-z\\.-]+)(${pattern})`, 'gi'), (m) => {
-                const k = m.match(new RegExp(pattern, 'i'))[0];
-                const v = manifest[k];
-                return m.replace(k, v).replace(/^((?:\.?\.?\/?)+)?/, _endsWith(config.cdn, '/') ? config.cdn : `${config.cdn}/`);
-              }))
-              .pipe(gulp.dest(config.rev.output))
-              .on('end', callback)
-              .on('error', callback);
-          } else {
-            gulp.src(config.rev.replace)
-              .pipe($replace(new RegExp(`${pattern}`, 'gi'), (m) => (manifest[m])))
-              .pipe(gulp.dest(config.rev.output))
-              .on('end', callback)
-              .on('error', callback);
-          }
-        });
-    })
-    .on('error', callback);
+            rimraf(`{${removables.join(',')}}`)
+                .then(() => {
+                    if (!_isEmpty(config.cdn)) {
+                        gulp.src(config.rev.replace)
+                            .pipe($replace(new RegExp(`((?:\\.?\\.\\/?)+)?([\\/\\da-z\\.-]+)(${pattern})`, 'gi'), (m) => {
+                                const k = m.match(new RegExp(pattern, 'i'))[0];
+                                const v = manifest[k];
+                                return m.replace(k, v).replace(/^((?:\.?\.?\/?)+)?/, _endsWith(config.cdn, '/') ? config.cdn : `${config.cdn}/`);
+                            }))
+                            .pipe(gulp.dest(config.rev.output))
+                            .on('end', callback)
+                            .on('error', callback);
+                    } else {
+                        gulp.src(config.rev.replace)
+                            .pipe($replace(new RegExp(`${pattern}`, 'gi'), (m) => (manifest[m])))
+                            .pipe(gulp.dest(config.rev.output))
+                            .on('end', callback)
+                            .on('error', callback);
+                    }
+                });
+        })
+        .on('error', callback);
 });
 
 // Compiles and deploys vendor javascript file.
 gulp.task('javascript:vendor', () => {
-  const b = browserify({
-    debug: config.env.debug,
-    transform: envify
-  });
+    const b = browserify({
+        debug: config.env.debug,
+        transform: envify
+    });
 
-  config.vendors.forEach((lib) => {
-    b.require(lib);
-  });
+    config.vendors.forEach((lib) => {
+        b.require(lib);
+    });
 
-  if (config.env.debug) {
-    b.bundle()
-      .on('error', $util.log)
-      .pipe(source('vendors.js'))
-      .pipe(buffer())
-      .pipe($size({ title: '[javascript:vendor]', gzip: true }))
-      .pipe(gulp.dest(config.javascripts.vendor.output))
-      .pipe(browserSync.stream({ match: '**/*.js' }));
-  } else {
-    b.bundle()
-      .on('error', $util.log)
-      .pipe(source('vendors.js'))
-      .pipe(buffer())
-      .pipe($size({ title: '[javascript:vendor]', gzip: true }))
-      .pipe(gulp.dest(config.javascripts.vendor.output));
-  }
+    if (config.env.debug) {
+        b.bundle()
+            .on('error', $util.log)
+            .pipe(source('vendors.js'))
+            .pipe(buffer())
+            .pipe($size({ title: '[javascript:vendor]', gzip: true }))
+            .pipe(gulp.dest(config.javascripts.vendor.output))
+            .pipe(browserSync.stream({ match: '**/*.js' }));
+    } else {
+        b.bundle()
+            .on('error', $util.log)
+            .pipe(source('vendors.js'))
+            .pipe(buffer())
+            .pipe($size({ title: '[javascript:vendor]', gzip: true }))
+            .pipe(gulp.dest(config.javascripts.vendor.output));
+    }
 });
 
 // Compiles and deploys main javascript file.
 gulp.task('javascript:main', () => {
-  let appBundler = browserify({
-    entries: [
-      config.javascripts.main.entry
-    ],
-    extensions: [
-      '.js',
-      '.jsx',
-      '.json'
-    ],
-    cache: {},
-    packageCache: {},
-    debug: config.env.debug,
-    fullPaths: config.env.debug
-  })
-  .transform('babelify', {
-    babelrc: true
-  })
-  .transform(envify({
-    _: 'purge',
-    NODE_ENV: config.env.debug ? 'development' : 'production',
-    DEMO: config.env.demo || false,
-    VERSION: pckg.version
-  }))
-  .external(config.vendors); // Specify all vendors as external source
+    let appBundler = browserify({
+        entries: [
+            config.javascripts.main.entry
+        ],
+        extensions: ['.js', '.jsx', '.json'],
+        cache: {},
+        packageCache: {},
+        debug: config.env.debug,
+        fullPaths: config.env.debug
+    })
+    .transform('babelify', {
+        babelrc: true
+    })
+    .transform(envify({
+        _: 'purge',
+        NODE_ENV: config.env.debug ? 'development' : 'production',
+        DEMO: config.env.demo || false,
+        VERSION: pckg.version
+    }))
+    .external(config.vendors); // Specify all vendors as external source
 
-  const rebundle = () => {
-    appBundler.bundle()
-      .on('error', $util.log)
-      .pipe(source('application.js'))
-      .pipe(buffer())
-      .pipe($size({ title: '[javascript:main]', gzip: true }))
-      .pipe(gulp.dest(config.javascripts.main.output))
-      .pipe(browserSync.stream({ match: '**/*.js' }));
-  };
+    const rebundle = () => {
+        appBundler.bundle()
+            .on('error', $util.log)
+            .pipe(source('application.js'))
+            .pipe(buffer())
+            .pipe($size({ title: '[javascript:main]', gzip: true }))
+            .pipe(gulp.dest(config.javascripts.main.output))
+            .pipe(browserSync.stream({ match: '**/*.js' }));
+    };
 
     if (config.env.debug) {
         appBundler = watchify(appBundler, {
@@ -271,24 +267,24 @@ gulp.task('javascript:main', () => {
 
 // Watch for file changes.
 gulp.task('watch', () => {
-  if (_has(config, 'watch.entries')) {
-    config.watch.entries.map((entry) => {
-      return gulp.watch(entry.files, { cwd: config.sourceDir }, entry.tasks);
-    });
-  }
+    if (_has(config, 'watch.entries')) {
+        config.watch.entries.map((entry) => {
+            return gulp.watch(entry.files, { cwd: config.sourceDir }, entry.tasks);
+        });
+    }
 
-  gulp.watch(['public/index.html', 'public/**/*.js']).on('change', () => {
-    browserSync.reload('*.html');
-  });
+    gulp.watch(['public/index.html', 'public/**/*.js']).on('change', () => {
+        browserSync.reload('*.html');
+    });
 });
 
 gulp.task('revision', () => {
-  const seq = ['rev'];
-  sequence(...seq);
+    const seq = ['rev'];
+    sequence(...seq);
 });
 
 gulp.task('header', () => {
-  const banner = `
+    const banner = `
 /** 
 * Builify - Static website creator by ${pckg.author}
 *
@@ -300,36 +296,36 @@ gulp.task('header', () => {
 */
 `;
 
-  return gulp.src([
-    path.join(config.javascripts.vendor.output, 'vendors.js'),
-    path.join(config.javascripts.main.output, 'application.js')
-  ])
-    .pipe($header(banner))
-    .pipe(gulp.dest(config.javascripts.main.output));
+    return gulp.src([
+        path.join(config.javascripts.vendor.output, 'vendors.js'),
+        path.join(config.javascripts.main.output, 'application.js'),
+    ])
+        .pipe($header(banner))
+        .pipe(gulp.dest(config.javascripts.main.output));
 });
 
 gulp.task('default', () => {
-  const seq = [
-    'html',
-    'images',
-    'files',
-    'files:template',
-    'files:template:thumbnail',
-    'stylesheet:main',
-    'stylesheet:canvas',
-    'javascript:vendor',
-    'javascript:main'
-  ];
+    const seq = [
+        'html',
+        'images',
+        'files',
+        'files:template',
+        'files:template:thumbnail',
+        'stylesheet:main',
+        'stylesheet:canvas',
+        'javascript:vendor',
+        'javascript:main',
+    ];
 
-  if (config.env.debug) {
-    seq.push('watch');
-    seq.push('server');
-  }
+    if (config.env.debug) {
+        seq.push('watch');
+        seq.push('server');
+    }
 
-  rimraf(config.buildDir)
-    .then(() => {
-      sequence(...seq);
-    }, (error) => {
-      throw error;
-    });
+    rimraf(config.buildDir)
+        .then(() => {
+            sequence(...seq);
+        }, (error) => {
+            throw error;
+        });
 });
